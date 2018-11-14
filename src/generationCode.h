3b3485108a03729b5782618d9dddc8beaa1b0759
Nurfe <lucien.klein@viacesi.fr>
2018-11-14 16:43:14 +0100

    Module

--- src/generationCode.h
+++ src/generationCode.h
@@ -2,9 +2,9 @@
 // Created by lucie on 14/11/2018.
 //
 
-#ifndef PROJETFONDAGRP8_GENERATIONCODE_H
-#define PROJETFONDAGRP8_GENERATIONCODE_H
+#ifndef __GENE__
+#define __GENE__
 
 void generationCode(int choix);
 
-#endif //PROJETFONDAGRP8_GENERATIONCODE_H
+#endif
